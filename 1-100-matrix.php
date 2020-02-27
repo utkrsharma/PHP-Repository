5e7dedb5d7ee52134800ece2452f495187bcff54
Utkarsh Sharma <61494859+utkrsharma@users.noreply.github.com>
2020-02-27 14:11:31 +0530

    Add files via upload

---
+++ 1-100-matrix.php
@@ -0,0 +1,15 @@
+<!-- <?php
+//$number = 9;
+//$str = "Beijing";
+//printf("There are %u million bicycles in %s.",$number,$str);
+?>
+ -->
+<?php
+for ($i=1;$i<=10;$i++) {
+    for ($j=$i;$j<=($i+90);$j+=10){ 
+        echo $j;
+        echo "\n";
+    }
+    echo "<br>";
+}   
+?>
